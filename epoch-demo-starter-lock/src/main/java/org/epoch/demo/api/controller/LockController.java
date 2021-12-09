87b67120f8e5110b26a60818d158dae63ac9a583
Marshal <marshal960818@gmail.com>
2021-12-09 22:33:59 +0800

    feat: improve demo for starter-lock

--- epoch-demo-starter-lock/src/main/java/org/epoch/demo/api/controller/LockController.java
+++ epoch-demo-starter-lock/src/main/java/org/epoch/demo/api/controller/LockController.java
@@ -1,11 +1,14 @@
 package org.epoch.demo.api.controller;
 
+import lombok.Data;
 import org.epoch.core.base.BaseController;
+import org.epoch.core.rest.Response;
 import org.epoch.core.rest.ResponseEntity;
-import org.epoch.demo.domain.service.LockService;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.epoch.starter.lock.annotation.Lock;
+import org.epoch.starter.lock.annotation.LockKey;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -13,13 +16,39 @@ import org.springframework.web.bind.annotation.RestController;
  * @date 2021/1/26
  */
 @RestController
-@RequestMapping("/demo")
+@RequestMapping("/demo/lock")
 public class LockController extends BaseController {
-    @Autowired
-    private LockService lockService;
 
-    @GetMapping
-    public ResponseEntity<Object> hello() throws InterruptedException {
-        return lockService.testLock();
+    @GetMapping("/test1")
+    @Lock(name = "'lockOps'")
+    public ResponseEntity<Object> test1() throws InterruptedException {
+        Thread.sleep(20000L);
+        return Response.success();
+    }
+
+    @GetMapping("/test11")
+    @Lock(name = "'lockOps'+#id")
+    public ResponseEntity<Object> test1(@RequestParam String id) throws InterruptedException {
+        Thread.sleep(20000L);
+        return Response.success();
+    }
+
+    @GetMapping("/test2")
+    @Lock(waitTime = 10L)
+    public ResponseEntity<Object> test2(@LockKey @RequestParam String id) throws InterruptedException {
+        Thread.sleep(20000L);
+        return Response.success();
+    }
+
+    @GetMapping("/test3")
+    @Lock(name = "", waitTime = 10L)
+    public ResponseEntity<Object> test3(@LockKey("userName") User user) throws InterruptedException {
+        Thread.sleep(20000L);
+        return Response.success();
+    }
+
+    @Data
+    public static class User {
+        String userName;
     }
 }
