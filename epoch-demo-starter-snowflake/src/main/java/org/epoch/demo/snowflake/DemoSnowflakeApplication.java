eb0d18548366d5c35f9eac080fe1c154a8a11ec2
Marshal <marshal960818@gmail.com>
2022-07-03 15:39:51 +0800

    feat:add demo for starter-snowflake

---
+++ epoch-demo-starter-snowflake/src/main/java/org/epoch/demo/snowflake/DemoSnowflakeApplication.java
@@ -0,0 +1,21 @@
+package org.epoch.demo.snowflake;
+
+import org.epoch.starter.core.util.ApplicationContextHolder;
+import org.epoch.starter.snowflake.helper.SnowflakeHelper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Marshal
+ * @since 2022/7/3
+ */
+@SpringBootApplication
+public class DemoSnowflakeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSnowflakeApplication.class, args);
+
+        SnowflakeHelper snowflakeHelper = ApplicationContextHolder.getContext().getBean(SnowflakeHelper.class);
+        long next = snowflakeHelper.next();
+        System.out.println("next key is: " + next);
+    }
+}
