7292e4653431571f0714610718cd3b94a4ba72a5
Marshal <marshal960818@gmail.com>
2022-07-14 23:07:57 +0800

    feat: add jpa sql configuration

--- epoch-demo-starter-jpa/src/main/java/org/epoch/demo/jpa/infrastructure/repository/entity/Role.java
+++ epoch-demo-starter-jpa/src/main/java/org/epoch/demo/jpa/infrastructure/repository/entity/Role.java
@@ -2,15 +2,10 @@ package org.epoch.demo.jpa.infrastructure.repository.entity;
 
 import java.util.Date;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
 import javax.persistence.Table;
 
 import lombok.Data;
-import org.epoch.jpa.annotation.LogicDelete;
 import org.epoch.jpa.domain.BaseAuditEntity;
-import org.hibernate.annotations.SQLDelete;
-import org.hibernate.annotations.Where;
 
 /**
  * @author Marshal
@@ -18,8 +13,6 @@ import org.hibernate.annotations.Where;
 @Data
 @Entity
 @Table(name = "sys_role")
-@Where(clause = "status = '1'")
-@SQLDelete(sql = "update role set status = '0' where id = ? ")
 public class Role extends BaseAuditEntity<String> {
     private String code;
     private String name;
